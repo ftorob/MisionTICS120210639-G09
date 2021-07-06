0d102f160336e5ebee8dfa65de8cc2894f9b59f9
ftorob <fernandotorobernal@gamil.com>
2021-07-05 20:49:12 -0500

    Actualziación comandos GIT

--- Ciclo 2/S120210630/src/App.java
+++ Ciclo 2/S120210630/src/App.java
@@ -10,7 +10,7 @@ public class App {
 
         //GIT & GITHUB
         /*
-        Algunos comandos importantes para trabajar en GIT son:
+        Algunos comandos importantes en orden para subir los archivos a GITHUB son:
         1) git --version (Da la versión)
         2) git init (Initialized empty Git repository in C:/Users/FERNANDO TORO/Documents/MisionTic2022/.git/)
         3) git status (Muestra los archivos o carpetas en la dirección inicializada)
@@ -18,7 +18,10 @@ public class App {
         5) git commit -m 'Archivos MisionTic2022' (sube todos los archivos a GITHUB pero se debe configurar usuario y email con )
         git config --global user.name "Your Name" y git config --global user.email
         6) git remote add origin https://github.com/ftorob/MisionTICS120210639-G09.git (ruta de repositorio)
-        7) git push -u origin main (cargue automático)
+        7) git push -u origin master (cargue automático)
+        Para poder subir todos los archivos fui a GITHUB --> SETTINGS --> REPOSITOIRES --> Cambie "MAIN" por "MASTER"
+        para poder arreglar el error del push "error: src refspec main does not match any
+        error: failed to push some refs to 'https://github.com/ftorob/MisionTICS120210639-G09.git'"
         */
     }
 }
